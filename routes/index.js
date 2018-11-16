cdf0d74c4c9e670dee55b79473704fcebcd8d71e
Jean-hugues Migeon <Jean-Hugues@MacBook-Pro-de-Jean-hugues.local>
2018-11-16 08:22:23 +0100

    final

--- routes/index.js
+++ routes/index.js
@@ -90,17 +90,19 @@ router.get('/populateVA2', function(req, res, next) {
                     return;
                 }
                 var params = {
-                    TableName:"SERVEME2",
                     Item:{
                         "pKey": uuid.v4(),
                         "toolName": "vulnerabilities2",
-                        "vulnId": item[0],
-                        "osvdbTitle": item[1],
-                        "IP": item[2],
-                        "formattedRisk": item[3],
-                        "firstSeenDate": item[4],
-                        "status": item[6]
-                    }
+                        "osvdbTitle": item[0],
+                        "IP": item[1],
+                        "status": item[2],
+                        "lastSeenDate": item[3],
+                        "assignedToUser": item[4],
+                        "firstSeenDate": item[5],
+                        "vulnId": item[6],
+                        "formattedRisk": item[7]
+                    },
+                    TableName:"SERVEME2"
                 };
                 docClient.put(params, function (err, data) {
                     if (err) {
@@ -129,18 +131,20 @@ router.get('/populateVA3', function(req, res, next) {
                 if(index == 0) {
                     return;
                 }
+                //osvdbTitle,IP,status,lastSeenDate,ports,assetId,firstSeenDate,vulnId,formattedRisk
                 var params = {
-                    TableName:"SERVEME2",
                     Item:{
                         "pKey": uuid.v4(),
                         "toolName": "vulnerabilities3",
-                        "vulnId": item[0],
-                        "osvdbTitle": item[1],
-                        "IP": item[2],
-                        "formattedRisk": item[3],
-                        "firstSeenDate": item[4],
-                        "status": item[6]
-                    }
+                        "osvdbTitle": item[0],
+                        "IP": item[1],
+                        "status": item[2],
+                        "lastSeenDate": item[3],
+                        "firstSeenDate": item[6],
+                        "vulnId": item[7],
+                        "formattedRisk": item[8]
+                    },
+                    TableName:"SERVEME2"
                 };
                 docClient.put(params, function (err, data) {
                     if (err) {
